17058c829b4d99fa339753dc38718aeb50be0c4d
Ilija Q <79657913+IlijaQ@users.noreply.github.com>
2024-09-08 16:34:46 +0200

    Privacy page updated

--- Pages/Privacy.cshtml.cs
+++ Pages/Privacy.cshtml.cs
@@ -10,6 +10,8 @@ namespace Online_Book_Store.Pages
 {
     public class PrivacyModel : PageModel
     {
+        private string ContactEmail { get; set; }
+
         private readonly ILogger<PrivacyModel> _logger;
 
         public PrivacyModel(ILogger<PrivacyModel> logger)
@@ -19,6 +21,17 @@ namespace Online_Book_Store.Pages
 
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            ContactEmail = GetEmail();
+            return Redirect($"mailto:{ContactEmail}");
+        }
+
+        private string GetEmail()
+        {
+            return "kujovicilija@gmail.com";
         }
     }
 }
